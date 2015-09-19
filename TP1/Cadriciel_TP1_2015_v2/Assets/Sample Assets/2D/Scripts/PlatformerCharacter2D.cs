c41bc9b72a1ba890cd023973f7addfea5589b039
unknown <juhue@L4818-17.gigl.polymtl.ca>
2015-09-19 18:03:17 -0400

    What is jump xD

--- TP1/Cadriciel_TP1_2015_v2/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ TP1/Cadriciel_TP1_2015_v2/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -5,34 +5,46 @@ public class PlatformerCharacter2D : MonoBehaviour
 {
 	bool facingRight = true;							// For determining which way the player is currently facing.
 
-	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
-	[SerializeField] float jumpForce = 10f;			// Amount of force added when the player jumps.	
+	[SerializeField] float maxSpeed = 10f;					// The fastest the player can travel in the x axis.
+	[SerializeField] float jumpForce = 10f;					// Amount of force added when the player jumps.	
+	[SerializeField] float jumpWallHorizontalForce = 50f;		// Amount of force added when the player jumps when he touches the wall. This is an offset to the X component
+	[SerializeField] float jetpackForce = 1f;			//
+	[SerializeField] bool showJumpLine = false;			// A boolean marking the maximum jump distance relative to the character
 
 	[Range(0, 1)]
 	[SerializeField] float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 
-
-	[SerializeField] float jumpTime = 0.2f;				// Maximum jump time
+	[Range(0.01f, 0.2f)]
+	[SerializeField] float jumpTime = 0.05f;			// Maximum jump time
 	[SerializeField] float floatingFactor = .5f;		// Floating factor when the character is moving in the air
 	[SerializeField] LayerMask whatIsGround;			// A mask determining what is ground to the character
-	[SerializeField] int maximumJumps = 1;			// A mask determining what is ground to the character
+	[SerializeField] LayerMask whatIsWall;				// A mask determining what is wall to the character
+	[SerializeField] int maximumJumps = 1;				// A mask determining what is ground to the character
+
 	
-	Transform groundCheck;								// A position marking where to check if the player is grounded.
-	float groundedRadius = .2f;							// Radius of the overlap circle to determine if grounded
-	bool grounded = false;								// Whether or not the player is grounded.
 	Transform ceilingCheck;								// A position marking where to check for ceilings
-	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
-	Animator anim;										// Reference to the player's animator component.
-	bool isJumping = false; 							// Character is currently jumping
-	
-	float jumpTimer = 0;
+	Transform groundCheck;								// A position marking where to check if the player is grounded
+	Transform wallCheck;								// A position marking where to check for walls
+
 	int jumpCounter = 0;
+
+	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
+	float groundedRadius = .05f;						// Radius of the overlap circle to determine if grounded
+	float wallRadius  = 0.2f;							// Radius of the overlap circle to determine if touching the wall									
+	float jumpTimer = 0;
+
+	bool grounded = false;								// Whether or not the player is grounded.
+	bool isTouchingWall = false;						// Whether or not the player is touching the wall
+	bool isJumping = false; 							// Character is currently jumping
+
+	Animator anim;										// Reference to the player's animator component.
 	
     void Awake()
 	{
 		// Setting up references.
 		groundCheck = transform.Find("GroundCheck");
 		ceilingCheck = transform.Find("CeilingCheck");
+		wallCheck = transform.Find("WallCheck");
 		anim = GetComponent<Animator>();
 	}
 
@@ -45,12 +57,16 @@ public class PlatformerCharacter2D : MonoBehaviour
 
 		// Set the vertical animation
 		anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
-	}
 
+		if (showJumpLine) {
+			//float maxJumpDist = (jumpForce - Physics.gravity.y) * Mathf.Pow(jumpTime, 2);
+			float maxJumpDist = Mathf.Pow(jumpForce, 2)/(2 * Physics.gravity.y);
+			Debug.DrawLine (new Vector3 (rigidbody2D.position.x - 200, rigidbody2D.position.y + maxJumpDist), new Vector3 (rigidbody2D.position.x + 200, rigidbody2D.position.y + maxJumpDist), Color.green, 0, true);
+		}
+	}
 
 	public void Move(float move, bool crouch, bool jump)
 	{
-
 		// If crouching, check to see if the character can stand up
 		if (!crouch && anim.GetBool ("Crouch")) {
 			// If the character has a ceiling preventing them from standing up, keep them crouching
@@ -58,23 +74,17 @@ public class PlatformerCharacter2D : MonoBehaviour
 				crouch = true; 
 		}
 
-		// Set whether or not the character is crouching in the animator
-		anim.SetBool ("Crouch", crouch);
 
 		//only control the player if grounded or floatingFactor is bigger than 0
 		if (grounded || floatingFactor > 0) {
+			// Set whether or not the character is crouching in the animator
+			anim.SetBool ("Crouch", crouch);
+			
 			// Reduce the speed if crouching by the crouchSpeed multiplier
 			move = (crouch ? move * crouchSpeed : move);
-
+			
 			// The Speed animator parameter is set to the absolute value of the horizontal input.
 			anim.SetFloat ("Speed", Mathf.Abs (move));
-
-			// Move the character differently if in the air based on the floating factor.
-			if (!grounded) {
-				rigidbody2D.velocity = new Vector2 (move * maxSpeed * floatingFactor, rigidbody2D.velocity.y);
-			} else {
-				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
-			}
 
 			// If the input is moving the player right and the player is facing left...
 			if (move > 0 && !facingRight)
@@ -86,18 +96,34 @@ public class PlatformerCharacter2D : MonoBehaviour
 				Flip ();
 		}
 
+		if (grounded) { // not in the air
+
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		} else if (floatingFactor > 0) {
+
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed * floatingFactor, rigidbody2D.velocity.y);
+
+			isTouchingWall = Physics2D.OverlapCircle (wallCheck.position, wallRadius, whatIsWall);
+		}
+
 		// If the player should jump...
+		if (((jumpCounter < maximumJumps && !isJumping) || isTouchingWall) && jump) {
 
+			if (!isTouchingWall) {
+				jumpCounter++;
+			} 
 
-		if (jumpCounter < maximumJumps && jump && !isJumping) {
-			jumpCounter++;
-			Debug.Log (jumpCounter);
 			isJumping = true;
+	
 			StartCoroutine (JumpRoutine ());
+		} else if (jumpCounter == maximumJumps && CrossPlatformInput.GetButton ("Jump")) {
+			Debug.Log ("Jetpack");
+			rigidbody2D.AddForce (new Vector2 (0f, jetpackForce));
 		}
 
 		if (grounded && !isJumping) {
-			Debug.Log("I am grounded!");
+			Debug.Log ("I am grounded and I reset jump counter");
 			jumpCounter = 0;
 		}
 	}
@@ -116,18 +142,33 @@ public class PlatformerCharacter2D : MonoBehaviour
 	// http://gamasutra.com/blogs/DanielFineberg/20150825/244650/Designing_a_Jump_in_Unity.php
 	IEnumerator JumpRoutine()
 	{
+
+
+		float xComponent = 0f;
+		if (isTouchingWall) {
+			xComponent = jumpWallHorizontalForce;
+
+			if(facingRight)
+			{
+				xComponent = -xComponent;
+			}
+			Flip ();
+		}
+
 		rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 0);
 
 		jumpTimer = 0;
 		
 		// Check if jump button is still pressed
 		while(CrossPlatformInput.GetButton ("Jump") && jumpTimer < jumpTime){
-			
 			// Jump time proportion
 			float proportionCompleted = jumpTimer / jumpTime;
 			
 			Vector2 thisFrameJumpVector = Vector2.Lerp (new Vector2 (0f, jumpForce), Vector2.zero, proportionCompleted);
+
+			thisFrameJumpVector += Vector2.Lerp ( Vector2.zero,new Vector2 (xComponent, 0f), proportionCompleted);
 			
+
 			// Increment the force relative to the time spent with the jump button pressed
 			rigidbody2D.AddForce (thisFrameJumpVector);
 			
@@ -135,7 +176,6 @@ public class PlatformerCharacter2D : MonoBehaviour
 			yield return null;
 		}
 			isJumping = false;
-			Debug.Log("I have finished jumping!");
 		
 	}
 }
