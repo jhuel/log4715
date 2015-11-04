7f6f918549c79b78ab85a013a6df17d1856f1f7b
pat <pat@patrick.com>
2015-11-03 22:15:51 -0500

    Yoadfoasdfo

---
+++ TP1/Cadriciel_TP2_2015/Jeu de course/Assets/Sample Assets/Vehicles/Car/Scripts/CarGUI.cs
@@ -0,0 +1,51 @@
+﻿/* Source for speedometer : http://answers.unity3d.com/questions/15414/how-can-i-make-an-on-screen-speedometer.html */
+
+using UnityEngine;
+
+[RequireComponent(typeof(GUIText))]
+public class CarGUI : MonoBehaviour
+{
+
+    public CarController car;                   // reference to the car controller to get the information needed for the display
+    private const float MphtoMps = 2.237f;      // constant for converting miles per hour to metres per second
+    private string display =          			// template string for GUI info
+        	"{0:0} mph \n" +
+            "Gear: {1:0}/{2:0}\n" +
+            "Revs {3:0%}\n" +
+            "Throttle: {4:0%}\n";
+
+	public Texture2D texture = null;
+	public float angle = 0;
+	public Vector2 size = new Vector2(128, 128);
+	Vector2 pos = new Vector2(0, 0);
+	Rect rect;
+	Vector2 pivot;
+	
+	void Start() {
+		UpdateSettings();
+	}
+
+	void UpdateSettings() {
+		pos = new Vector2(transform.localPosition.x, transform.localPosition.y);
+		rect = new Rect(pos.x, pos.y, size.x, size.y);
+		pivot = new Vector2(rect.xMin + rect.width * 0.5f, rect.yMin + rect.height * 0.5f);
+	}
+	
+    void Update()
+    {
+        //object[] args = new object[] { car.CurrentSpeed * MphtoMps, car.GearNum + 1, car.NumGears, car.RevsFactor, car.AccelInput };
+
+        // display the car gui information
+        // guiText.text = string.Format(display, args);
+    }
+
+	void OnGUI() {
+		angle = (car.CurrentSpeed / car.MaxSpeed) * 180;
+
+		if (Application.isEditor) { UpdateSettings(); }
+		Matrix4x4 matrixBackup = GUI.matrix;
+		GUIUtility.RotateAroundPivot(angle, pivot);
+		GUI.DrawTexture(rect, Texture2D.whiteTexture);
+		GUI.matrix = matrixBackup;
+	}
+}
---
+++ tuto/Assets/scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	
+	private Rigidbody rb;
+	private int count; //number of power ups
+
+	// Called at start on first frame
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+	void setCountText(){
+	
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 6) {
+			winText.text = "You win nigger";
+		}
+
+	}
+	void Update()
+	{
+
+
+	}
+
+	// PHYSICS
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText();
+		}
+	}
+}
