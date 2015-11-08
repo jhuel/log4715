343bdbdb8c69c3973175c86fade5e95e026dba38
unknown <juhue@L4818-06.gigl.polymtl.ca>
2015-11-07 21:11:55 -0500

    bonus accelerateur xD

---
+++ TP1/Cadriciel_TP2_2015/Jeu de course/Assets/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    // https://unity3d.com/learn/tutorials/projects/roll-a-ball/creating-collectables?playlist=17141
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
